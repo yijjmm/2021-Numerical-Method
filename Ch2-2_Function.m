bd8ad54cc5cc19127b308f6217abb5b907fba41a
Jimin Lee <47144126+yijjmm@users.noreply.github.com>
2021-10-11 04:26:41 +0900

    Create Ch2-2_Function.m

---
+++ Ch2-2_Function.m
@@ -0,0 +1,29 @@
+%Function 표현 4
+
+function pengsu() 
+    a = input ("first?"); % 첫번째 값을 a에 입력. scanf 역할
+    b = input ("second?");
+    c = sub1 (a, b);
+    d = sub2 (a, b);
+
+    fprintf('%d + %d = %d\n', a, b, c); % 출력. printf 역할
+    fprintf('%d + %d = %d\n', a, b, d);
+end
+
+function z = sub1(x, y) 
+    z = x + y;
+end
+
+function z = sub2(x, y) 
+    z = x - y;
+end
+
+-------------------------------------------------
+
+% 명령창 호출
+
+>> pengsu
+first?4
+second?7
+4 + 7 = 11
+4 + 7 = -3
